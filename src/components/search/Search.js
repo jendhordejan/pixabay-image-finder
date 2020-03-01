fc06d1aadd8ab5f147b7e811e31a7d453f925db6
Nicole <nicole@chhays-air.lan>
2020-03-01 15:05:46 +0100

    completed resultAmount feature in Search

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import TextField from "material-ui/TextField"; //equivent to input tag
-import SelectField from "material-ui/SelectField";
+import TextField from "@material-ui/core/TextField"; //equivent to input tag
+import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import axios from "axios";
 
 class Search extends Component {
   state = {
     searchText: "",
-    amount: 15, //the amount of pictures to be loaded
+    searchResults: 15, //the amount of pictures to be loaded
     apiUrl: "https://pixabay.com/api",
     apiKey: "15441363-37973e460f704d66535a8884a",
     images: [] //array container for the fetched pictures from Pixabay
@@ -16,7 +16,7 @@ class Search extends Component {
   handleSubmit = () => {
     axios
       .get(
-        `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+        `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.searchResults}&safesearch=true`
       )
       .then(res => this.setState({ images: res.data.hits })) //loading the fetched data from api into our local state
       .catch(err => console.log(err));
@@ -25,6 +25,14 @@ class Search extends Component {
   handleOnChange = event => {
     this.setState({ [event.target.name]: event.target.value });
     this.handleSubmit();
+  };
+
+  handleOnAmountChange = (event, index, value) => {
+    console.log(`eventname: ${event}, value: ${value}`);
+
+    console.log(event);
+
+    this.setState({ searchResults: event.target.value });
   };
 
   render() {
@@ -40,18 +48,18 @@ class Search extends Component {
         />
 
         <br />
-        <SelectField //for selecting the amount of pictures to be displayed
-          name="amount"
+        <Select //for selecting the amount of pictures to be displayed
+          name="searchResults"
           floatingLabelText="Amount"
-          value={this.state.amount}
-          onChange={this.handleOnChange}
+          value={this.state.searchResults}
+          onChange={this.handleOnAmountChange}
         >
           <MenuItem value={5}>5</MenuItem>
           <MenuItem value={10}>10</MenuItem>
           <MenuItem value={15}>15</MenuItem>
           <MenuItem value={30}>30</MenuItem>
           <MenuItem value={50}>50</MenuItem>
-        </SelectField>
+        </Select>
         <br />
       </div>
     );
